e835bd861dd43b3ea2287650b7ed0a6f98eb38c6
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-09-12 10:39:20 +0100

    Style title text

--- App.tsx
+++ App.tsx
@@ -1,15 +1,23 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { AppLoading } from "expo";
 import { StyleSheet, Text, View } from "react-native";
+import { useFonts, FugazOne_400Regular } from "@expo-google-fonts/fugaz-one";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Qwizzr</Text>
+  let [fontsLoaded] = useFonts({ FugazOne_400Regular });
+  let fontSize = 40;
 
-      <StatusBar style="auto" />
-    </View>
-  );
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.titleText}>Qwizzr</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -18,5 +26,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#ffe6f4",
     alignItems: "center",
     justifyContent: "center",
+  },
+  titleText: {
+    fontFamily: "FugazOne_400Regular",
+    fontSize: 50,
   },
 });
