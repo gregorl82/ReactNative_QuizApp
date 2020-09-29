0fb2bd1646a9b758bbb5e8684a17b9877a7ef133
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-09-29 19:50:45 +0100

    Test button press

--- src/components/Button.test.tsx
+++ src/components/Button.test.tsx
@@ -1,15 +1,27 @@
 import React from "react";
-import { render } from "react-native-testing-library";
+import { render, fireEvent } from "react-native-testing-library";
 import { Button } from "./Button";
 
 const mockOnPress = jest.fn();
 
 describe("Button component", () => {
+  beforeEach(() => jest.clearAllMocks());
+
   it("renders correctly", () => {
     const { getByText } = render(
       <Button buttonText="test" onPress={mockOnPress} />
     );
 
     getByText("TEST");
+  });
+
+  it("responds to press", () => {
+    const { getByText } = render(
+      <Button buttonText="test" onPress={mockOnPress} />
+    );
+
+    fireEvent.press(getByText("TEST"));
+
+    expect(mockOnPress).toHaveBeenCalled();
   });
 });
