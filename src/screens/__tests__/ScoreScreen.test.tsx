f333e76e9bcffea26342089a72afe04e75892d9b
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-10-24 12:24:16 +0100

    Complete score screen tests

--- src/screens/__tests__/ScoreScreen.test.tsx
+++ src/screens/__tests__/ScoreScreen.test.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ScoreScreen } from "../index";
-import { render, waitFor } from "@testing-library/react-native";
+import { render, waitFor, fireEvent } from "@testing-library/react-native";
+import "@testing-library/jest-native/extend-expect";
 import { MockedNavigator } from "../../utils/mockedNavigator";
 
 const mockAnsweredQuestion1 = {
@@ -41,7 +42,7 @@ describe("Score screen", () => {
       <MockedNavigator
         component={ScoreScreen}
         params={{
-          score: 0,
+          score: 5,
           answeredQuestions: [mockAnsweredQuestion1, mockAnsweredQuestion2],
         }}
       />
@@ -49,11 +50,79 @@ describe("Score screen", () => {
     await waitFor(() => getByText("5 out of 10!"));
   });
 
-  xit("clicking 'show results' displays the score breakdown modal", () => {});
+  xit("clicking 'show results' displays the score breakdown modal", async () => {
+    const { getByText, queryByText } = render(
+      <MockedNavigator
+        component={ScoreScreen}
+        params={{
+          score: 1,
+          answeredQuestions: [mockAnsweredQuestion1, mockAnsweredQuestion2],
+        }}
+      />
+    );
+    await waitFor(() => getByText("1 out of 10!"));
 
-  xit("clicking 'close' closes modal and shows score screen again", () => {});
+    fireEvent.press(getByText("SHOW RESULTS"));
+    await waitFor(() => getByText("Results"));
+    expect(queryByText("1 out of 10!")).toBeNull();
+  });
 
-  xit("if a question was answered correctly, question number and 'CORRECT!' are shown in modal", () => {});
+  xit("clicking 'close' closes modal and shows score screen again", async () => {
+    const { getByText, queryByText } = render(
+      <MockedNavigator
+        component={ScoreScreen}
+        params={{
+          score: 1,
+          answeredQuestions: [mockAnsweredQuestion1, mockAnsweredQuestion2],
+        }}
+      />
+    );
+    await waitFor(() => getByText("1 out of 10!"));
 
-  xit("if a question was anwswered wrong, question number, 'WRONG!', users answer and correct answer are shown in modal", () => {});
+    fireEvent.press(getByText("SHOW RESULTS"));
+    await waitFor(() => getByText("Results"));
+    expect(queryByText("1 out of 10!")).toBeNull();
+
+    fireEvent.press(getByText("CLOSE"));
+    await waitFor(() => getByText("1 out of 10!"));
+    expect(queryByText("Results")).toBeNull();
+  });
+
+  xit("if a question was answered correctly, question number and 'CORRECT!' are shown in modal", async () => {
+    const { getByText } = render(
+      <MockedNavigator
+        component={ScoreScreen}
+        params={{
+          score: 1,
+          answeredQuestions: [mockAnsweredQuestion1, mockAnsweredQuestion2],
+        }}
+      />
+    );
+    await waitFor(() => getByText("1 out of 10!"));
+
+    fireEvent.press(getByText("SHOW RESULTS"));
+    await waitFor(() => getByText("Results"));
+
+    getByText("Question 1 - CORRECT!");
+  });
+
+  xit("if a question was anwswered wrong, question number, 'WRONG!', users answer and correct answer are shown in modal", async () => {
+    const { getByText } = render(
+      <MockedNavigator
+        component={ScoreScreen}
+        params={{
+          score: 1,
+          answeredQuestions: [mockAnsweredQuestion1, mockAnsweredQuestion2],
+        }}
+      />
+    );
+    await waitFor(() => getByText("1 out of 10!"));
+
+    fireEvent.press(getByText("SHOW RESULTS"));
+    await waitFor(() => getByText("Results"));
+
+    getByText("Question 2 - WRONG!");
+    getByText(`Your answer: ${mockAnsweredQuestion2.selectedAnswer}`);
+    getByText(`Correct answer: ${mockAnsweredQuestion2.correctAnswer}`);
+  });
 });
